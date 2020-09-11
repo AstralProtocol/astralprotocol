13c84254fff087963b7188c8a560e465cd803b7e
j-mars <jldcmartins@gmail.com>
2020-09-11 18:13:40 +0100

    contract changes

--- packages/astral-protocol-contracts/contracts/SpatialAssetRegistrar.sol
+++ packages/astral-protocol-contracts/contracts/SpatialAssetRegistrar.sol
@@ -8,35 +8,34 @@ contract SpatialAssetRegistrar is Ownable {
     //----------------- Events ------------------
 
     event CreateGeoDID(address caller, bytes32 hash, string cid);
-    event UpdateHash(address caller, bytes32 hash);
-    event UpdateCID(address caller, string cid);
-    event DeleteGeoDID(address caller);
+    event UpdateGeoDID(address caller, bytes32 hash, string cid);
+    event DeleteGeoDID(bytes32 hash);
 
     //----------------- Storage -----------------
 
-    struct geoDID {
-        bool exist;
-        // 32 bytes hash after the did method name
-        bytes32 hash;
-        // content id on IPFS
-        string cid;
+    /*
+    struct assetProvider {
+        address
+        bytes32 did;
     }
+    mapping 
+    */
 
-    // an address (DID controller) registers a geoDID
-    mapping(address => geoDID[]) geoDIDs;
-    // current Id of the array in the mapping
-    mapping(address => uint256) currentGeoDIDId;
+    // did:astral:3122bi312ibuo312gb31i23bi12 (hash: 3122bi312ibuo312gb31i23bi12)
+    // an address (DID controller) can register multiple geoDIDs through a bytes32 hash
+    mapping(address => mapping(bytes32 => string)) geoDIDs;
+
+    mapping(bytes32 => bool) geoDidExists;
 
     //---------------- Modifiers -----------------
 
-    modifier nonCreatedGeoDID(bytes32 hash) {
-        require(geoDIDs[msg.sender][currentGeoDIDId[msg.sender]].hash != hash, 'The geoDID hash was created in the past and was deactivated');
+    modifier nonCreatedGeoDID(bytes32 _hash) {
+        require(!geoDidExists[_hash], 'The geoDID hash was already created');
         _;
     }
 
-    modifier onlyValidGeoDID(address _addr, bytes32 hash) {
-        require(geoDIDs[_addr][currentGeoDIDId[_addr]].exist, "geoDID does not exist");
-        require(geoDIDs[msg.sender][currentGeoDIDId[msg.sender]].hash == hash, 'The geoDID hash does not match the registry');
+    modifier onlyValidGeoDID(bytes32 _hash) {
+        require(geoDidExists[_hash], "geoDID does not exist");
         _;
     }
 
@@ -46,47 +45,44 @@ contract SpatialAssetRegistrar is Ownable {
     //---------------- Public functions -----------------
 
     // make this function payable and require an amount
-    function register(bytes32 _hash, string memory _cid) public {
-        if (geoDIDs[msg.sender][currentGeoDIDId[msg.sender]].exist) {
-            updateHash(_hash);
-            updateCID(_cid);
-        } else {
-            createGeoDID(_hash, _cid);
-        }
+    function register(bytes32 _hash, string memory _cid) public nonCreatedGeoDID(_hash){
+        createGeoDID(_hash, _cid);
     }
 
-    function deregister(address _toBeDeleted) public onlyOwner {
-        deleteGeoDID(_toBeDeleted);
+        // make this function payable and require an amount
+    function update(bytes32 _previousHash, bytes32 _hash, string memory _cid) public onlyValidGeoDID(_previousHash) {
+        updateGeoDID(_previousHash, _hash, _cid);
+    }
+
+
+    function deregister(bytes32 _hash) public onlyOwner {
+        deleteGeoDID(_hash);
     }
 
     // geoDID resolver that anyone in the internet can call to resolve the geoDID
-    function geoDIDResolver(address _addr, bytes32 _hash) public view onlyValidGeoDID(_addr, _hash) returns (string memory) {
-        return geoDIDs[_addr][currentGeoDIDId[msg.sender]].cid;
+    function geoDIDResolver(address _addr, bytes32 _hash) public view onlyValidGeoDID(_hash) returns (string memory) {
+        return geoDIDs[_addr][_hash];
     }
 
     //---------------- Internal functions -----------------
 
-    function createGeoDID(bytes32 _hash, string memory _cid) internal nonCreatedGeoDID(_hash) {
-        geoDIDs[msg.sender][currentGeoDIDId[msg.sender]] = geoDID(true, _hash, _cid);
+    function createGeoDID(bytes32 _hash, string memory _cid) internal {
+        geoDIDs[msg.sender][_hash] = _cid;
 
         emit CreateGeoDID(msg.sender, _hash, _cid);
-
-        currentGeoDIDId[msg.sender]++;
     }
 
-    function updateHash(bytes32 _hash) internal {
-        geoDIDs[msg.sender][currentGeoDIDId[msg.sender]].hash = _hash;
-        emit UpdateHash(msg.sender, _hash);
+    function updateGeoDID(bytes32 _previousHash, bytes32 _hash, string memory _cid) internal {
+        geoDidExists[_previousHash] = false;
+        geoDIDs[msg.sender][_hash] = _cid;
+        geoDidExists[_hash] = true;
+        emit UpdateGeoDID(msg.sender, _hash, _cid);
     }
 
-    function updateCID(string memory _cid) internal {
-        geoDIDs[msg.sender][currentGeoDIDId[msg.sender]].cid = _cid;
-        emit UpdateCID(msg.sender, geoDIDs[msg.sender][currentGeoDIDId[msg.sender]].cid);
-    }
 
-    function deleteGeoDID(address _addr) internal {
-        geoDIDs[_addr][currentGeoDIDId[_addr]].exist = false;
-        emit DeleteGeoDID(_addr);
+    function deleteGeoDID(bytes32 _hash) internal {
+        geoDidExists[_hash] = false;
+        emit DeleteGeoDID(_hash);
     }
 
 }
