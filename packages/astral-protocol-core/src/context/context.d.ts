6d6a7cf67831fbbe6480c70a0666752313b0483a
Jared Childers <jared.childers97@gmail.com>
2020-10-13 13:06:26 -0400

    UPdate

--- packages/astral-protocol-core/src/context/context.d.ts
+++ packages/astral-protocol-core/src/context/context.d.ts
@@ -1,6 +1,6 @@
 import { Powergate } from "../pinning/powergate";
 import { Resolver } from "did-resolver";
-import { AstralClient } from "../astral-client";
+import AstralClient from "../astral-client";
 import { DID } from "dids";
 export interface Context {
     did?: DID;
