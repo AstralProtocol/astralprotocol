6187362f9204c487f8fe0fa7d490237c0e23f0ad
ronerlih <ronerlih@users.noreply.github.com>
2024-06-13 21:54:29 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+[![Github Actions][gha-badge]][gha] [![Foundry][foundry-badge]][foundry] [![License: MIT][license-badge]][license]
+[![codecov][codecov-badge]][codecov]
+[![Twitter](https://img.shields.io/twitter/follow/astralprotocol?style=social)](https://twitter.com/astralprotocol)
+
+[gitpod-badge]: https://img.shields.io/badge/Gitpod-Open%20in%20Gitpod-FFB45B?logo=gitpod
+[gha]: https://github.com/AstralProtocol/astralprotocol/actions
+[gha-badge]: https://github.com/AstralProtocol/astralprotocol/actions/workflows/ci.yml/badge.svg
+[foundry]: https://getfoundry.sh/
+[foundry-badge]: https://img.shields.io/badge/Built%20with-Foundry-FFDB1C.svg
+[license]: https://opensource.org/licenses/MIT
+[license-badge]: https://img.shields.io/badge/License-MIT-blue.svg
+[codecov]: https://codecov.io/gh/AstralProtocol/astralprotocol
+[codecov-badge]: https://codecov.io/gh/AstralProtocol/astralprotocol/graph/badge.svg?token=8MPT8407FS
+
+![Astral-jambo](https://github.com/AstralProtocol/astralprotocol/assets/7093389/e2f31237-1259-47e8-8830-d8caebff9f47)
+
+# Astral Protocol
+
+Deploy, extend, and maintain GeoLocation apps in a decentralized environment. Visit [astral.global](https://astral.global).
+
+## Documentation
+
+Check out the Astral Protocol Docs hosted on [gitbook](https://astral-protocol.gitbook.io/astral).
+
+## Design
+
+![Astral-diagram](https://github.com/AstralProtocol/astralprotocol/assets/7093389/562936df-c748-4527-a743-33f6520bac64)
+
+## ❤️ Contribute
+
+Please check our [contributing guide](https://github.com/AstralProtocol/astralprotocol/wiki/Contributing-guidelines)! We deeply appreciate
+any help, review, suggestion!
+
+pr changes to test, (expected to be the only change in the new pr)
---
+++ legacy/packages/core/src/astral-client.ts
@@ -0,0 +1,168 @@
+import { Powergate } from './pin/powergate';
+import { Document } from './docu/document';
+import GeoDIDResolver from './resolver/geo-did-resolver';
+import { DIDDocument, Resolver, ServiceEndpoint } from 'did-resolver';
+import {
+    GeoDidType,
+    IDocumentInfo,
+    IPinInfo,
+    IAsset,
+    ILoadInfo,
+    IAssetInfo,
+} from './geo-did/interfaces/global-geo-did-interfaces';
+
+export { GeoDidType, IDocumentInfo, IPinInfo, IAsset, ILoadInfo } from './geo-did/interfaces/global-geo-did-interfaces';
+
+export class AstralClient {
+    document: Document;
+
+    constructor(public _ethereumAddress: string, public _thegraphEndpoint: string, public _host: string, public _token: string) {
+        this.document = new Document(_ethereumAddress);
+    }
+
+    static async build(
+        _ethereumAddress: string,
+        _thegraphEndpoint = 'https://api.thegraph.com/subgraphs/name/astralprotocol/spatialassetsfinalv1',
+        _host = 'http://52.168.51.215:6002',
+        _token?: string,
+    ): Promise<AstralClient> {
+        try {
+            const powergate = await Powergate.build(_host, _token);
+            let token = '';
+            if (_token) token = _token;
+            else token = await powergate.getToken();
+
+            return new AstralClient(_ethereumAddress, _thegraphEndpoint, _host, token);
+        }catch(e){
+            throw e;
+        }
+    }
+
+    getInstanceToken(): string {
+        return this._token;
+    }
+
+    async createGenesisGeoDID(_typeOfGeoDID: string, assets?: IAsset[]): Promise<IDocumentInfo> {
+        try {
+            const response: IDocumentInfo = await this.document.addGenesisDocument(_typeOfGeoDID, this._host, this._token, assets);
+            return response;
+        } catch (e) {
+            console.log('Unable to initialize');
+            throw e;
+        }
+    }
+
+    async createChildGeoDID(
+        _typeOfGeoDID: string,
+        _parentID: string,
+        _path: string,
+        assets?: IAsset[],
+    ): Promise<IDocumentInfo> {
+        try {
+            const response: IDocumentInfo = await this.document.addChildDocument(_typeOfGeoDID, _parentID, _path, this._host, this._token, assets);
+            return response;
+        } catch (e) {
+            console.log('Unable to initialize');
+            throw e;
+        }
+    }
+
+    async pinDocument(documentInfo: IDocumentInfo): Promise<IPinInfo> {
+        try {
+            const pinDate: Date = new Date();
+            const powergate = await Powergate.build(this._host, this._token);
+            const stringdoc = JSON.stringify(documentInfo.documentVal);
+            const uint8array = new TextEncoder().encode(stringdoc);
+            const cid: string = await powergate.getAssetCid(uint8array);
+            await powergate.pin(cid);
+
+            return { geodidid: documentInfo.geodidid, cid: cid, pinDate: pinDate, token: this._token };
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    }
+
+    async pinAsset(docId: string, powergate: Powergate, asset: IAsset): Promise<ServiceEndpoint> {
+        try {
+            const uint8array = new TextEncoder().encode(asset.data);
+            const seCID: string = await powergate.getAssetCid(uint8array);
+            await powergate.pin(seCID);
+
+            return {
+                id: docId.concat(`#${asset.tag}`),
+                type: asset.type,
+                serviceEndpoint: seCID,
+            };
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    }
+
+    // Add Assets to an item. Validation happens
+    async addAssetsToItem(docId: string, assets: IAsset[]): Promise<IDocumentInfo> {
+        let response: ILoadInfo;
+        let service: ServiceEndpoint;
+
+        try {
+            response = await this.loadDocument(docId);
+            const document_Info: IDocumentInfo = response.documentInfo;
+
+            if (document_Info.documentVal.didmetadata.type.toLowerCase() == GeoDidType.Item) {
+                for (let i = 0; i < assets.length; i++) {
+                    service = await this.pinAsset(docId, response.powergateInstance, assets[i]);
+                    await document_Info.documentVal.service.push(service);
+                }
+            } else {
+                throw new Error(
+                    'Unfortunately the Document ID you provided is not of Item type, so you cannot add any Assets to this Document. Please try again with a valid GeoDID Item',
+                );
+            }
+            return document_Info;
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    }
+
+    // TODO: Read/Load a GeoDID Document
+    async loadDocument(docId: string): Promise<ILoadInfo> {
+        try {
+            const powergate = await Powergate.build(this._host, this._token);
+            const geoDidResolver = GeoDIDResolver.getResolver(this, powergate);
+            const didResolver = new Resolver(geoDidResolver);
+            const doc: DIDDocument = await didResolver.resolve(docId);
+
+            return { documentInfo: { geodidid: docId, documentVal: doc }, powergateInstance: powergate };
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    }
+
+    // must have a fragment and token of the
+    async loadAsset(doc: DIDDocument, docId: string): Promise<IAssetInfo> {
+        try {
+            const powergate = await Powergate.build(this._host, this._token);
+            
+            const services: Array<ServiceEndpoint> = doc.service;
+
+            for (let i = 0; i < services.length; i++) {
+                if (services[i].id == docId) {
+                    const asset_cid = services[i].serviceEndpoint;
+                    var bytes: Uint8Array = await powergate.getFromPowergate(asset_cid);
+                    var uint8array = new TextDecoder().decode(bytes);
+                    var type = services[i].type;
+                    break;
+                }
+            }
+
+            return {id: docId, type: type, data: uint8array};
+
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    }
+}
