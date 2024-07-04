48aab751b5edb432167602e42c21b3168258a6df
John X <john.iv@x25bd.com>
2024-07-04 18:00:31 +0100

    Deploy website - based on 23c6a8e35cea83ba5f69ae8db74a3179b174ac1f

--- legacy/packages/core/src/geo-did/default/geo-did-default-factory.ts
+++
@@ -1,9 +0,0 @@
-import { IAbstractGeoDIDFactory } from '../interfaces/factory-interfaces';
-
-export class ConcreteDefaultFactory implements IAbstractGeoDIDFactory{
-    // Creates the Document
-    public async createGeoDIDDocument<T>(document: {new (): T}): Promise<T>{
-        return new document();
-    }
-
-}
--- legacy/packages/ipld-encoded-geotiff/__tests__/ipld-encoded-geotiff.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const ipldEncodedGeotiff = require('..');
-
-describe('ipld-encoded-geotiff', () => {
-    it('needs tests');
-});
